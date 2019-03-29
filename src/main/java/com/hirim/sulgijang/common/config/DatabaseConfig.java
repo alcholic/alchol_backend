087ddb6d61b1b984d10e5b9f37b0aeac568161c8
Limah.lee <nb11892@navercorp.com>
2019-03-29 17:13:56 +0900

    mybatis config

--- src/main/java/com/hirim/sulgijang/common/config/DatabaseConfig.java
+++ src/main/java/com/hirim/sulgijang/common/config/DatabaseConfig.java
@@ -1,20 +1,17 @@
 package com.hirim.sulgijang.common.config;
 
-import com.zaxxer.hikari.HikariDataSource;
 import org.apache.commons.dbcp2.BasicDataSource;
-import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.Configuration;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
-import org.mybatis.spring.mapper.MapperScannerConfigurer;
+
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-
-import java.io.IOException;
 
 @MapperScan(basePackages = "com.hirim.sulgijang.repositories")
 @Configuration
@@ -55,10 +52,17 @@ public class DatabaseConfig {
     }
 
     @Bean
-    public SqlSessionFactoryBean sqlSessionFactory(ApplicationContext applicationContext) throws IOException {
+    public SqlSessionFactoryBean sqlSessionFactory(ApplicationContext applicationContext) throws Exception {
         final SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
         sqlSessionFactoryBean.setDataSource(dataSource());
+        sqlSessionFactoryBean.setTypeAliasesPackage("com.hirim.sulgijang.models");
         sqlSessionFactoryBean.setMapperLocations(applicationContext.getResources(mapperLocation));
+
+
+        Configuration configuration = new Configuration();
+        configuration.setMapUnderscoreToCamelCase(true);
+        sqlSessionFactoryBean.setConfiguration(configuration);
+
         return sqlSessionFactoryBean;
     }
 }
